300c75f0e6e030a50018a005c6987a6bf5b1ab06
nvtung194208 <tung.nv194208@sis.hust.edu.vn>
2023-02-17 01:12:53 +0700

    init project

---
+++ models/user.model.js
@@ -0,0 +1,10 @@
+// File path: ./models/user.model.js
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  telegramId: String,
+})
+
+const User = mongoose.model('User', schema)
+
+module.exports = User
